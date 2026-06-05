0c10eef0b13f31eb2257a1965b0b59641fc64386
Cesar Varela <cesar.a.varela.g@gmail.com>
2026-06-05 14:24:39 +0000

    feat: add PdfServiceError domain type

---
+++ src/errors.ts
@@ -0,0 +1,16 @@
+export type PdfErrorCode =
+  | 'INVALID_XML'
+  | 'MISSING_TIMBRE'
+  | 'UNSUPPORTED_TYPE'
+  | 'RENDER_ERROR';
+
+export class PdfServiceError extends Error {
+  constructor(
+    message: string,
+    public readonly code: PdfErrorCode,
+    public readonly statusCode: number,
+  ) {
+    super(message);
+    this.name = 'PdfServiceError';
+  }
+}
